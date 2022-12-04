fed6d68a51e7c0a77d251154f614302e96a00591
My Pham <muricapham@icloud.com>
2022-12-03 21:27:40 -0500

    Code cleanup

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,11 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Filter.css";
 
 const Filter = ({ fetchFilter, name, handleClose }) => {
-  // const [{ name, color }] = pokemonTypes.filter((item) => item.name === type);
-
-  // const imgUrl = require(`/src/assets/pokemonTypes/${name}.svg`);
-
   return (
     <button
       className="filter-button"
@@ -14,7 +10,6 @@ const Filter = ({ fetchFilter, name, handleClose }) => {
         fetchFilter(`${name}`);
       }}
     >
-      {/* <img src={imgUrl} width={16} height={16} alt={name} color={color} /> */}
       {name}
     </button>
   );
